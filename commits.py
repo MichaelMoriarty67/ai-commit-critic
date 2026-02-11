b44fe55e042da35e1fcd2326f00a12893edcfa55
MichaelMoriarty67 <michaelmoriarty67@hotmail.com>
2026-02-10 22:55:25 -0500

    added fn get_last_n_commits

--- commits.py
+++ commits.py
@@ -5,17 +5,27 @@ from typing import List
 
 
 def get_last_n_commits(n: int, repo: Repo) -> List[Commit]:
-    commits: List[Commit] = []
+    commit_data: List[Commit] = []
 
     commits = list(repo.iter_commits("HEAD", max_count=n))
 
     for commit in commits:
         msg = commit.message
-        diff = commit.diff(commit.parents[0], creat_patch=True)
-        print(diff)
+
+        parent = commit.parents[0] if commit.parents else None
+        diffs = commit.diff(parent, create_patch=True)
+
+        diff_str = ""
+        for diff in diffs:
+            diff_str += diff.diff.decode("utf-8")
+
+        commit_data.append(Commit(message=msg, diff=diff_str))
+
+    return commit_data
 
 
 if __name__ == "__main__":
     repo_path = "./"
     repo = Repo(repo_path)
-    get_last_n_commits(10, repo)
+    commits = get_last_n_commits(10, repo)
+    print(commits)
--- schemas.py
+++ schemas.py
@@ -3,3 +3,4 @@ from pydantic import BaseModel
 
 class Commit(BaseModel):
     message: str
+    diff: str
